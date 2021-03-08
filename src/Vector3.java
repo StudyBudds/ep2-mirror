f9084713b25c6f3e844f70f6c3cc314c3673b2f2
Daniel Fenz <leinadznef@gmail.com>
2021-03-08 18:57:32 +0100

    Implemented all required Vector3 methods

--- src/Vector3.java
+++ src/Vector3.java
@@ -10,62 +10,62 @@ public class Vector3 {
 
     //TODO: define constructor.
 
+    public Vector3() {
+
+    }
+
+    public Vector3(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
     // Returns the sum of this vector and vector 'v'.
     public Vector3 plus(Vector3 v) {
-
-        //TODO: implement method.
-        return null;
+        return new Vector3(this.x + v.x, this.y + v.y, this.z + v.z);
     }
 
     // Returns the product of this vector and 'd'.
     public Vector3 times(double d) {
-
-        //TODO: implement method.
-        return null;
+        return new Vector3(this.x * d, this.y * d, this.z * d);
     }
 
     // Returns the sum of this vector and -1*v.
     public Vector3 minus(Vector3 v) {
-
-        //TODO: implement method.
-        return null;
+        return new Vector3(this.x - v.x, this.y - v.y, this.z - v.z);
     }
 
     // Returns the Euclidean distance of this vector
     // to the specified vector 'v'.
     public double distanceTo(Vector3 v) {
-
-        //TODO: implement method.
-        return -1d;
+        return minus(v).length();
     }
 
     // Returns the length (norm) of this vector.
     public double length() {
-
-        //TODO: implement method.
-        return 0;
+        return Math.sqrt(Math.pow(this.x + this.y + this.z, 2));
     }
 
     // Normalizes this vector: changes the length of this vector such that it becomes 1.
     // The direction and orientation of the vector is not affected.
     public void normalize() {
-
-        //TODO: implement method.
+        double length = length();
+        this.x /= length;
+        this.y /= length;
+        this.z /= length;
     }
 
     // Draws a filled circle with a specified radius centered at the (x,y) coordinates of this vector
     // in the existing StdDraw canvas. The z-coordinate is not used.
     public void drawAsDot(double radius, Color color) {
-
-        //TODO: implement method.
+        StdDraw.setPenColor(color);
+        StdDraw.filledCircle(this.x, this.y, radius);
     }
 
     // Returns the coordinates of this vector in brackets as a string
     // in the form "[x,y,z]", e.g., "[1.48E11,0.0,0.0]".
     public String toString() {
-
-        //TODO: implement method.
-        return "";
+        return "[" + this.x + "," + this.y + "," + this.z + "]";
     }
 
 }
