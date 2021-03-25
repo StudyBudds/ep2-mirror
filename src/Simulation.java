370cef2d1b82c2fa3f3acbe5035b71aa8ab6226d
Daniel Fenz <e12019861@student.tuwien.ac.at>
2021-03-25 16:08:31 +0100

    reset simulation to state before ab2

--- src/Simulation.java
+++ src/Simulation.java
@@ -12,57 +12,45 @@ public class Simulation {
     public static void main(String[] args) {
 
 
-//        Body sun = new Body("Sol",
-//                1.989e30,
-//                696340e3,
-//                new Vector3(),
-//                new Vector3(),
-//                StdDraw.YELLOW);
-//
-//        Body mercury = new Body("Mercury",
-//                3.301e23,
-//                2.4397e3,
-//                new Vector3(-46.0e9, 0, 0),
-//                new Vector3(0, -47.87e3, 0),
-//                StdDraw.RED);
-//
-//        Body earth = new Body("Earth",
-//                5.972e24,
-//                6371e3,
-//                new Vector3(148e9, 0, 0),
-//                new Vector3(0, 29.29e3, 0),
-//                StdDraw.BLUE);
-//
-//        Body jupiter = new Body("Jupiter",
-//                1898.19e24,
-//                71492e3,
-//                new Vector3(740.522e9, 0, 0),
-//                new Vector3(0, 13.72e3, 0),
-//                StdDraw.ORANGE);
-//        // moon does not really work as planned :(
-//        Body moon = new Body("Moon",
-//                7.342e22,
-//                1738.1e3,
-//                new Vector3(148e9 + 6371e3 + 405.4e8, 0.4055e9, 0),
-//                new Vector3(0, 29.29e3 + 1.022e3, 0),
-//                StdDraw.LIGHT_GRAY);
+        Body sun = new Body("Sol",
+                1.989e30,
+                696340e3,
+                new Vector3(),
+                new Vector3(),
+                StdDraw.YELLOW);
+
+        Body mercury = new Body("Mercury",
+                3.301e23,
+                2.4397e3,
+                new Vector3(-46.0e9, 0, 0),
+                new Vector3(0, -47.87e3, 0),
+                StdDraw.RED);
+
+        Body earth = new Body("Earth",
+                5.972e24,
+                6371e3,
+                new Vector3(148e9, 0, 0),
+                new Vector3(0, 29.29e3, 0),
+                StdDraw.BLUE);
+
+        Body jupiter = new Body("Jupiter",
+                1898.19e24,
+                71492e3,
+                new Vector3(740.522e9, 0, 0),
+                new Vector3(0, 13.72e3, 0),
+                StdDraw.ORANGE);
+        // moon does not really work as planned :(
+        Body moon = new Body("Moon",
+                7.342e22,
+                1738.1e3,
+                new Vector3(148e9 + 6371e3 + 405.4e8, 0.4055e9, 0),
+                new Vector3(0, 29.29e3 + 1.022e3, 0),
+                StdDraw.LIGHT_GRAY);
         // arbitrary initialisation: position opposite to the earth with maximal distance.
         // viewing from z direction movement is counter-clockwise
-        Body sun = new Body("Sol",1.989e30,696340e3,new Vector3(0,0,0),new Vector3(0,0,0),StdDraw.YELLOW);
-        Body earth = new Body("Earth",5.972e24,6371e3,new Vector3(-1.394555e11,5.103346e10,0),new Vector3(-10308.53,-28169.38,0),StdDraw.BLUE);
-        Body mercury = new Body("Mercury",3.301e23,2440e3,new Vector3(-5.439054e10,9.394878e9,0),new Vector3(-17117.83,-46297.48,-1925.57),StdDraw.GRAY);
-        Body venus = new Body("Venus",4.86747e24,6052e3,new Vector3(-1.707667e10,1.066132e11,2.450232e9),new Vector3(-34446.02,-5567.47,2181.10),StdDraw.PINK);
-        Body mars = new Body("Mars",6.41712e23,3390e3,new Vector3(-1.010178e11,-2.043939e11,-1.591727E9),new Vector3(20651.98,-10186.67,-2302.79),StdDraw.RED);
 
-        CosmicSystem bodies = new CosmicSystem("Sonnensystem");
-        bodies.add(sun);
-        bodies.add(earth);
-        bodies.add(mercury);
-        bodies.add(venus);
-        bodies.add(mars);
-        System.out.println(bodies.add(mars));
-
-        Vector3[] forceOnBody = new Vector3[bodies.size()];
+        Body[] bodies = new Body[] {earth, sun, mercury, jupiter};
+        Vector3[] forceOnBody = new Vector3[bodies.length];
 
         StdDraw.setCanvasSize(500, 500);
         StdDraw.setXscale(-2*AU,2*AU);
@@ -75,27 +63,26 @@ public class Simulation {
         StdDraw.clear(StdDraw.BLACK);
 
         double seconds = 0;
-        int length = bodies.size();
+
         // simulation loop
         while(true) {
 
             seconds++; // each iteration computes the movement of the celestial bodies within one second.
 
             // for each body (with index i): compute the total force exerted on it.
-            for (int i = 0; i < length; i++) {
+            for (int i = 0; i < bodies.length; i++) {
                 forceOnBody[i] = new Vector3(0, 0, 0); // begin with zero
-                Body curr = bodies.get(i);
-                for (int j = 0; j < length; j++) {
+                for (int j = 0; j < bodies.length; j++) {
                     if (i == j) continue;
-                    Vector3 forceToAdd = curr.gravitationalForce(bodies.get(j));
+                    Vector3 forceToAdd = bodies[i].gravitationalForce(bodies[j]);
                     forceOnBody[i] = forceOnBody[i].plus(forceToAdd);
                 }
             }
             // now forceOnBody[i] holds the force vector exerted on body with index i.
 
             // for each body (with index i): move it according to the total force exerted on it.
-            for (int i = 0; i < length; i++) {
-               bodies.get(i).move(forceOnBody[i]);
+            for (int i = 0; i < bodies.length; i++) {
+                bodies[i].move(forceOnBody[i]);
             }
 
             // show all movements in StdDraw canvas only every 3 hours (to speed up the simulation)
@@ -104,8 +91,8 @@ public class Simulation {
                 StdDraw.clear(StdDraw.BLACK);
 
                 // draw new positions
-                for (int i = 0; i < length; i++) {
-                    bodies.get(i).draw();
+                for (int i = 0; i < bodies.length; i++) {
+                    bodies[i].draw();
                 }
 
                 // show new positions
